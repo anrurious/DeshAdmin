0b487db232de07728d932193b5517b355e375cc8
ankured182 <ankured182@yahoo.com>
2016-04-27 17:32:45 +0600

    New Solution Uploaded
    Desh Admin

---
+++ DeshAdmin/Models/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeshAdmin.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+
+    }
+}
